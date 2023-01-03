e7f8711e8d6aea07e3acde65038dfe05cf7b3e34
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-01-03 11:28:56 +0100

    Update window

--- myApp.js
+++ myApp.js
@@ -1,18 +1,16 @@
 const {getAuthenticatedFetch} = require("./lib/client-credentials");
-const { menubar } = require('menubar');
+const {menubar} = require('menubar');
 const path = require("path");
 const fsExtra = require("fs-extra");
-const {getRDFasJson, turtleToN3Store} = require("./lib/utils");
-const { DataFactory } = require('n3');
-const { namedNode, literal, defaultGraph, quad } = DataFactory;
+const {removeToday, getAllDates, getVacationsToday} = require("./lib/utils");
 const {ipcMain} = require('electron');
 
 main();
 
 async function main() {
-  let dates = ['2023-12-23'];
   const mb = menubar({
     showDockIcon: false,
+    icon: path.join(process.cwd(), 'icon.png'),
     browserWindow: {
       webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
@@ -21,26 +19,30 @@ async function main() {
   });
 
   mb.on('ready', async () => {
+    let upcoming;
+    let vacationsToday;
     console.log('app is ready');
-    ipcMain.handle('dates', () => dates);
-    // your app code here
+    ipcMain.handle('upcoming', () => upcoming);
+    ipcMain.handle('vacationsToday', () => vacationsToday);
 
     const configFilePath = path.join(process.cwd(), 'config.json');
-    const {vacationUrl} = await fsExtra.readJson(configFilePath);
+    const {vacations} = await fsExtra.readJson(configFilePath);
     const fetch = await getAuthenticatedFetch(configFilePath);
-    //const data = await getRDFasJson(vacationUrl, {}, fetch);
-    const response = await fetch(vacationUrl, {headers: {'accept': 'text/turtle'}});
-    const store = await turtleToN3Store(await response.text());
-    dates = store.getObjects(null, namedNode('https://data.knows.idlab.ugent.be/person/office/#date'), null).map(a => a.value);
-    console.log(dates);
+    const data = await getAllDates(vacations, fetch);
+    upcoming = data;
+    data[0].dates.push('2023-01-03');
+    console.log(data);
+    vacationsToday = getVacationsToday(data)
+    console.log(vacationsToday);
+    removeToday(upcoming);
   });
 
   mb.on('after-create-window', () => {
     const mainWindow = mb.window;
-
   });
 
   mb.once('show', function () {
     //mb.window.openDevTools();
   });
 }
+
--- preload.js
+++ preload.js
@@ -4,6 +4,6 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
-  dates: () => ipcRenderer.invoke('dates'),
-  // we can also expose variables, not just functions
+  upcoming: () => ipcRenderer.invoke('upcoming'),
+  vacationsToday: () => ipcRenderer.invoke('vacationsToday')
 })
